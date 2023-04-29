4adcacb04d0ba968ed7f4dc90511614f4e126009
NicoDVKa <nicodvkcss@gmail.com>
2023-04-29 06:52:14 -0300

    Create Db, add WorkerService in ASPNET

--- Infrastructure/Persistence/AppDbContext.cs
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -1,10 +1,27 @@
 ﻿
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace Infrastructure.Persistence
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Suggestion> Suggestions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            modelbuilder.Entity<Suggestion>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+                entity.Property(c => c.Id).ValueGeneratedOnAdd();
+                entity.Property(c => c.MainUser).IsRequired();
+                entity.Property(c => c.SuggestedUser).IsRequired();
+                entity.Property(c => c.DateView).IsRequired(false);
+                entity.Property(c => c.View).IsRequired().HasDefaultValue(false);
+            });
+        }
     }
 }
